437f37c7288eda644ab924226177975c9c8ddfa9
sokainakanwal <25582@students.riphah.edu.pk>
2023-03-26 01:17:42 -0700

    Init

---
+++ FoodsApp-starting-code/app/src/main/java/com/haerul/foodsapp/view/category/CategoryView.java
@@ -0,0 +1,13 @@
+
+package com.haerul.foodsapp.view.category;
+
+import com.haerul.foodsapp.model.Meals;
+
+import java.util.List;
+
+public interface CategoryView {
+    void showLoading();
+    void hideLoading();
+    void setMeals(List<Meals.Meal> meals);
+    void onErrorLoading(String message);
+}
